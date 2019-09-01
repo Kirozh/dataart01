ef4ace8df56156742cafc6f478bd249290bee8a7
Кирилл О <cuprum.oxigen@gmail.com>
2019-09-02 00:58:56 +0300

    add data format

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ConsoleApp4
 {
@@ -6,9 +7,13 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            long d1 = new DateTime(1996,12,21, 20, 00, 00, new CultureInfo("en-US", false).Calendar).Ticks;
+            DateTime d = new DateTime(d1);
+            string format = "{0} 's Birthday is at {1:dd/MM/yyyy hh:mm:ss tt}";
+            
             Console.WriteLine("Hello World!");
             Console.WriteLine(" {0:c}", 123456);
-
+            Console.WriteLine(format, "kirill", d);
         }
     }
 }
