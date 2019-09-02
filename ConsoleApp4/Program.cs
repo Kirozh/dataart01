4085bb30c457a18e0360c6f97ddca0c2b0359abe
Кирилл О <cuprum.oxigen@gmail.com>
2019-09-02 12:59:49 +0300

    add sring functions

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -14,6 +14,19 @@ namespace ConsoleApp4
             Console.WriteLine("Hello World!");
             Console.WriteLine(" {0:c}", 123456);
             Console.WriteLine(format, "kirill", d);
+
+            char[] carry = {'e','l','e','v','a','t','o','r' };
+            string s =new string(carry);
+            string sc = new string("refregerator");
+            Console.WriteLine(s.Length);
+            Console.WriteLine(s.CompareTo(sc));
+                Console.WriteLine(s.Contains("e"));
+                Console.WriteLine(s.PadLeft(19, '1'));
+            Console.WriteLine(sc.PadRight(10,'1'));
+            s = s + ' ' + sc;
+            string[] darray = s.Split(' ');
+            foreach (string f in darray)
+                Console.WriteLine(f);
         }
     }
 }
